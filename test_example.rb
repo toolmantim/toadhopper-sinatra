c6601e43bec45fe6b1284679d406b7ab27b7e023
Tim Lucas <t.lucas@toolmantim.com>
2010-01-28 14:33:16 +1100

    Rewrote the test example in exemplor - still along way to go

--- test_example.rb
+++ test_example.rb
@@ -1,12 +1,3 @@
-require 'test/unit'
-
-require 'rr'
-class Test::Unit::TestCase
-  include RR::Adapters::TestUnit
-end
-
-require 'rack/test'
-
 toadhopper_dir = "#{File.dirname(__FILE__)}/../toadhopper"
 raise "#{toadhopper_dir} does not exist" unless File.directory?(toadhopper_dir)
 $:.unshift "#{toadhopper_dir}/lib"
@@ -14,51 +5,53 @@ $:.unshift "#{toadhopper_dir}/lib"
 $:.unshift "#{File.dirname(__FILE__)}/lib"
 
 require "#{File.dirname(__FILE__)}/example"
-
 Sinatra::Application.set :environment, :production
 
-class Sinatra::ToadHopper::TestExample < Test::Unit::TestCase
-  
+require 'exemplor'
+
+require 'rack/test'
+require 'rr'
+
+eg.helpers do
   include Rack::Test::Methods
-
-  def app; Sinatra::Application end
-
-  def setup
-    @toadhopper = ::ToadHopper.new("")
-    stub(::ToadHopper).new do |api_key|
-      @api_key = api_key
-      @toadhopper
-    end
-    stub(@toadhopper).post! do |*args|
-      @error, @options, @header = *args
-    end
-
-    post "/register", :name => "Billy", :password => "Bob"
+  include RR::Adapters::RRMethods
+  def app
+    Sinatra::Application
   end
+end
 
-  def test_api_key_set
-    assert_equal "apikey", @api_key
-  end
-  def test_filter_set
-    assert_equal [/password/], @toadhopper.filters
-  end
-  def test_reported_error
-    assert_equal RuntimeError, @error.class
-    assert_equal "Kaboom!", @error.message
-  end
-
-  def test_options
-    assert_equal ENV.to_hash,                          @options[:environment]
-    assert_equal "http://example.org/register",        @options[:url]
-    assert_equal({"name"=>"Billy", "password"=>"Bob"}, @options[:request].params)
-    assert_equal "production",                         @options[:framework_env]
-    assert_equal app.root,                             @options[:project_root]
-    assert_equal({:user_id => 42},                     @options[:session])
-    assert_equal "toadhopper-sinatra",                 @options[:notifier_name]
+eg.setup do
+  RR.reset
+  
+  @toadhopper = ::ToadHopper.new("")
+  stub(::ToadHopper).new do |api_key|
+    @api_key = api_key
+    @toadhopper
   end
   
-  def test_header_options
-    assert_equal "toadhopper-sinatra", @header['X-Hoptoad-Client-Name']
+  stub(@toadhopper).post! do |*args|
+    @error, @options, @header = *args
   end
+end
+
+eg "Posting" do
+  post "/register", :name => "Billy", :password => "Bob"
   
+  Check(@api_key).is("apikey")
+
+  Check(@toadhopper.filters).is([/password/])
+
+  Check(@error.class).is(RuntimeError)
+  Check(@error.message).is("Kaboom!")
+
+  Check(@options[:environment]).is(ENV.to_hash)
+  Check(@options[:url]).is("http://example.org/register")
+  Check(@options[:request].params).is({"name"=>"Billy", "password"=>"Bob"})
+  
+  Check(@options[:framework_env]).is("production")
+  Check(@options[:project_root]).is(app.root)
+  Check(@options[:session]).is({:user_id => 42})
+  Check(@options[:notifier_name]).is("toadhopper-sinatra")
+
+  Check(@header['X-Hoptoad-Client-Name']).is("toadhopper-sinatra")
 end
